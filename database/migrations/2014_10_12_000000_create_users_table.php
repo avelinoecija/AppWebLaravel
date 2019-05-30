35846c36f8ffda7aa6b8eccf63ab5e3c4b5d677f
avelinoecija <43956938+avelinoecija@users.noreply.github.com>
2019-05-30 16:43:25 +0200

    Add files via upload

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    protected $primaryKey = 'id';
+
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('curso');
+            $table->string('puntuacion');
+            $table->string('nivel');
+            $table->foreign('idpregunta')->references('idpregunta')->on('preguntas')->onUpdate('cascade');
+            $table->foreign('idrespuesta')->references('id')->on('respuestas')->onUpdate('cascade');
+            $table->boolean('admin')->default(false);
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
